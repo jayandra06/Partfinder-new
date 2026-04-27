709a04e7bd4cb34d0ac638dff8274e939f9b26f5
jayandra06 <68438965+jayandra06@users.noreply.github.com>
2026-04-27 12:58:40 +0530

    Delete PartFinder directory

--- PartFinder/PartFinder/Models/GridColumnDefinition.cs
+++
@@ -1,8 +0,0 @@
-namespace PartFinder.Models;
-
-public sealed class GridColumnDefinition
-{
-    public required string Key { get; init; }
-    public required string Header { get; init; }
-    public double Width { get; init; } = 160;
-}
--- PartFinder/PartFinder/ViewModels/ViewModelBase.cs
+++
@@ -1,7 +0,0 @@
-using CommunityToolkit.Mvvm.ComponentModel;
-
-namespace PartFinder.ViewModels;
-
-public abstract partial class ViewModelBase : ObservableObject
-{
-}
